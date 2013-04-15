bba9b896796178404bc0783c4338369ff7a25cf7
Miao Fan <fanmiao.cslt.thu@gmail.com>
2013-04-15 14:10:38 +0800

    rename of top-down merge sort

--- Algorithms_Princeton/src/com/dolphinnlp/java/algorithms/core/MergeTD.java
+++ Algorithms_Princeton/src/com/dolphinnlp/java/algorithms/core/MergeTD.java
@@ -1,10 +1,23 @@
 package com.dolphinnlp.java.algorithms.core;
 
-public class MergeUB {
-	
+public class MergeTD {
+	//Divide and Conquer
 	public static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi)
 	{
+		for(int k = lo; k <= hi; k++)
+		{
+			aux[k] = a[k];
+		}
 		
+		int i = lo, j = mid + 1;
+		
+		for(int k = lo; k <= hi; k++)
+		{
+			if(i > mid)
+				a[k] = aux[j++];
+			else if(j > hi)
+				a[k] = aux[i++];
+		}
 	}
 	/**
 	 * @param args
